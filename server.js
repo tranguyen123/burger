908bf56a0fd8d61c203058ee9391bc193507c955
tra <nellyng2013@gmail.com>
2018-08-26 12:52:54 -0700

    yuo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
+app.use(methodOverride('X-HTTP-Method-Override'));
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
