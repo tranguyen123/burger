4913c4eb2a81bc8eb850fead39cb32c7146ffbbf
tra <nellyng2013@gmail.com>
2018-08-28 20:29:15 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
 
 }else {
   connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "b4dd973e2ad284",
-    password: "dbf0b3a3",
-    database: "heroku_d344185bc6a3b51"
+    host: "localhost",
+    user: "root",
+    password: "Jamel2018",
+    database: "burgers"
 });
 }
 
