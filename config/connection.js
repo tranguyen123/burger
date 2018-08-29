bfa5fee94a6f1e5186997ef9dc091f6a0e2932b8
tra <nellyng2013@gmail.com>
2018-08-28 20:16:00 -0700

    lkj

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection= mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-01.cleardb.net",
-  user: "b4dd973e2ad284",
-  password: "dbf0b3a3",
-  database: "heroku_d344185bc6a3b51"
+}else {
+  connection = mysql.createConnection({
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b4dd973e2ad284",
+    password: "dbf0b3a3",
+    database: "heroku_d344185bc6a3b51"
 });
+}
+
 
 connection.connect(function(err) {
   if (err) {
